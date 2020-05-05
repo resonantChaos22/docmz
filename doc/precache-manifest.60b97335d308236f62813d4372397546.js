5008054dc9f0b04335d051da34b2d18a4c5a6c00
Shreyash <46599052+resonantChaos22@users.noreply.github.com>
2020-05-05 21:39:30 +0530

    Doc Commit

---
+++ doc/precache-manifest.60b97335d308236f62813d4372397546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8e39d8f8159c815c98a8b9831d00a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e775afaf6676d6265239",
+    "url": "/static/css/main.2a2eb46b.chunk.css"
+  },
+  {
+    "revision": "a9f9f56c057d5aa72b38",
+    "url": "/static/js/2.ab2a5cf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab2a5cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e775afaf6676d6265239",
+    "url": "/static/js/main.704f398c.chunk.js"
+  },
+  {
+    "revision": "a5361f9933c8e35616f2",
+    "url": "/static/js/runtime-main.ba0f77d9.js"
+  }
+]);
